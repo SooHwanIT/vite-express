611482f98d0182f072424d7c5c2dcf94f9fb31de
hyunjung <poggo201103@gmail.com>
2025-04-01 18:28:27 +0900

    vercel 설정

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,9 @@ dotenv.config();
 
 const app = express();
 
-// CORS 미들웨어 사용
 app.use(cors({
-    origin: 'http://localhost:5173'  // 요청을 허용할 클라이언트의 주소
+    origin: process.env.FRONT_BASE_URL,
+    credentials: true
 }));
 app.use(express.json());
 app.use(morgan("dev"));
@@ -24,19 +24,10 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello from Express!, connection successful! " });
 });
 
-app.get("/api", (req, res) => {
-    res.json({ message: "api connection successful! " });
-});
-app.get("/api/hello", (req, res) => {
-    res.json({ message: "hello" });
-});
-
-// 로컬 개발용: Vercel 배포 시에는 실행되지 않음
-if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
         console.log(`✅ Server running on http://localhost:${PORT}`);
     });
-}
 
-module.exports = app;
+
+export default app;
