6792ddc22d99b0388ee99541132973287e451e47
hyunjung <poggo201103@gmail.com>
2025-04-01 18:39:01 +0900

    백엔드 주소 변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://vite-express-back.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
