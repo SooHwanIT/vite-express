f5436e104708f597fe35303eff6c69cffdd2c630
hyunjung <poggo201103@gmail.com>
2025-04-01 15:55:30 +0900

    /api 추가

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,12 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello from Express!, connection successful! " });
 });
 
+// ✅ 기본 페이지
+app.get("/api", (req, res) => {
+    res.json({ message: "api connection successful! " });
+});
+
+
 // 포트 설정 (환경변수 또는 기본값 5000)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
